de3d2e5102f3f50bfe4074206efc89a1d48bce86
Nicholas Lhoest <31844608+nicholaslhoest@users.noreply.github.com>
2020-11-20 17:53:26 +0100

    Created Python File
    New file witj poki codre

---
+++ poke.py
@@ -0,0 +1,13 @@
+poki = input("What number are you thinking between 1 - 4 Answer: ")
+
+if poki == "1":
+    print("what the fuck")
+elif poki == "2":
+    print("DUDE!")
+elif poki == "3":
+    print("go to bed")
+elif poki == "4":
+    print("go eat")
+else:
+    print("Choose another number")
+    ans = input()
